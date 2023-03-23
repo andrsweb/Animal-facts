0516faa86179e458f7e5039796e3ede932ba05d7
andrsweb <proflinervk@gmail.com>
2023-03-23 14:07:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+const App = () => {
+	const [count, setCount] = useState( 0 )
+
+	return (
+		<div className="App">
+
+		</div>
+	)
+}
+
+export default App
